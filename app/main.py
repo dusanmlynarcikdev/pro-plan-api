23a1369689fbf9e308afab13dd3d8374d35a08f0
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-03-24 11:17:29 +0100

    format

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter, Depends, FastAPI, status
 
-_users_tag = "Users"
-
 app = FastAPI(
     title="Pro Subscription Management REST API",
     servers=[
         {"url": "http://localhost", "description": "Local"},
     ],
-    swagger_ui_parameters={"operationsSorter": "alpha"}
+    swagger_ui_parameters={"operationsSorter": "alpha"},
 )
