49d747f3758f31b70570c7e1fcd4b924b282d416
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-03-25 13:59:58 +0100

    use anyio

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ from typing import AsyncGenerator, Generator
 
 from fastapi.testclient import TestClient
 from pytest import fixture
-from pytest_asyncio import fixture as asyncio_fixture
 from sqlalchemy_utils import create_database as sqlalchemy_create_database
 from sqlalchemy_utils import database_exists as sqlalchemy_database_exists
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -14,19 +13,21 @@ DATABASE_URL = "postgresql+psycopg://postgres:postgres@database:5432/test"
 database_engine = create_engine(DATABASE_URL)
 
 
-@fixture(scope="function")
+@fixture
 def client() -> Generator[TestClient]:
     with TestClient(app) as client:
         yield client
 
 
-@asyncio_fixture(scope="function")
+@fixture
 async def session() -> AsyncGenerator[AsyncSession]:
     async with get_session_factory(database_engine)() as session_:
         transaction = await session_.begin()
+        app.dependency_overrides[get_session] = lambda: session_
         try:
             yield session_
         finally:
+            app.dependency_overrides.pop(get_session, None)
             await transaction.rollback()
 
 
@@ -34,10 +35,3 @@ async def session() -> AsyncGenerator[AsyncSession]:
 def create_database() -> None:
     if not sqlalchemy_database_exists(DATABASE_URL):
         sqlalchemy_create_database(DATABASE_URL)
-
-
-@fixture(scope="function", autouse=True)
-def override_dependencies(session: AsyncSession) -> Generator[None]:
-    app.dependency_overrides[get_session] = lambda: session
-    yield
-    app.dependency_overrides.clear()
