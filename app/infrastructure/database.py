a48bd802f5aec6ca335cbf24525eb26b94c31242
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-03-25 17:54:03 +0100

    Add database connection

---
+++ app/infrastructure/database.py
@@ -0,0 +1,17 @@
+from os import getenv
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+DATABASE_URL = getenv("DATABASE_URL", "")
+
+engine = create_async_engine(DATABASE_URL)
+session_factory = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession]:
+    async with session_factory() as session:
+        yield session
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,36 @@
-from typing import Generator
+from typing import AsyncGenerator, Generator
 
+from dotenv import load_dotenv
 from fastapi.testclient import TestClient
 from pytest import fixture
+from sqlalchemy_utils import create_database, database_exists
+from sqlmodel.ext.asyncio.session import AsyncSession
 
+load_dotenv(".env.test")
+
+from app.infrastructure.database import DATABASE_URL, get_session
 from app.main import app
 
 
-@fixture(scope="function")
+@fixture
 def client() -> Generator[TestClient]:
-    yield TestClient(app)
+    with TestClient(app) as client:
+        yield client
+
+
+@fixture
+async def session() -> AsyncGenerator[AsyncSession]:
+    async for session_ in get_session():
+        transaction = await session_.begin()
+        app.dependency_overrides[get_session] = lambda: session_
+        try:
+            yield session_
+        finally:
+            app.dependency_overrides.pop(get_session, None)
+            await transaction.rollback()
+
+
+@fixture(scope="session", autouse=True)
+def prepare_database() -> None:
+    if not database_exists(DATABASE_URL):
+        create_database(DATABASE_URL)
