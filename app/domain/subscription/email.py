b3676b9be20e40c5b4b89098e9e71acbb6c215d1
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-03-26 13:28:35 +0100

    update email object

--- app/domain/subscription/email.py
+++ app/domain/subscription/email.py
@@ -1,2 +1,18 @@
+from dataclasses import dataclass
+
+from pydantic import EmailStr, TypeAdapter, ValidationError
+
+from app.domain.subscription.errors import InvalidEmail
+
+_EMAIL_TYPE_ADAPTER = TypeAdapter(EmailStr)
+
+
+@dataclass(frozen=True, slots=True)
 class Email:
-    pass
+    value: str
+
+    def __post_init__(self) -> None:
+        try:
+            _EMAIL_TYPE_ADAPTER.validate_python(self.value)
+        except ValidationError:
+            raise InvalidEmail()
--- app/domain/subscription/errors.py
+++ app/domain/subscription/errors.py
@@ -1,3 +1,8 @@
+class InvalidEmail(Exception):
+    def __init__(self) -> None:
+        super().__init__("Invalid email")
+
+
 class SubscriptionExpired(Exception):
     def __init__(self) -> None:
         super().__init__("Subscription expired")
