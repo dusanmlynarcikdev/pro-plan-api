2e6f31420973188e4bb7d329072730b8c29f18ff
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-03-26 13:58:40 +0100

    add email test

---
+++ tests/unit/domain/subscription/email_test.py
@@ -0,0 +1,16 @@
+from pytest import mark, raises
+
+from app.domain.subscription.email import Email
+from app.domain.subscription.errors import InvalidEmail
+
+
+def test_success() -> None:
+    result = Email("john@doe.com")
+
+    assert "john@doe.com" == result.value
+
+
+@mark.parametrize("email", ("", "john", "john@", "john@doe", "johndoe.com"))
+def test_invalid_email(email: str) -> None:
+    with raises(InvalidEmail):
+        Email(email)
