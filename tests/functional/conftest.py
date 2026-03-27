074f68b249185d1d366bbc6fa4a07838b2ac4e85
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-03-27 10:40:07 +0100

    run database migrations before tests

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,9 +1,12 @@
 from typing import AsyncGenerator, Generator
 
+from alembic import command
+from alembic.config import Config
+from alembic.util.exc import CommandError
 from dotenv import load_dotenv
 from fastapi.testclient import TestClient
 from pytest import fixture
-from sqlalchemy_utils import create_database, database_exists
+from sqlalchemy_utils import create_database, database_exists, drop_database
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 load_dotenv(".env.test")
@@ -33,5 +36,15 @@ async def session() -> AsyncGenerator[AsyncSession]:
 
 @fixture(scope="session", autouse=True)
 def prepare_database() -> None:
-    if not database_exists(DATABASE_URL):
-        create_database(DATABASE_URL)
+    alembic_config = Config("alembic.ini")
+    alembic_config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+    if database_exists(DATABASE_URL):
+        try:
+            command.check(alembic_config)
+            return
+        except CommandError:
+            drop_database(DATABASE_URL)
+
+    create_database(DATABASE_URL)
+    command.upgrade(alembic_config, "head")
