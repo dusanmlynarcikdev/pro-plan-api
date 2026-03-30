c956c3c1623200051e7a7e2e78f308964b1d2279
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-03-30 16:36:00 +0200

    type

--- app/domain/subscription/repository.py
+++ app/domain/subscription/repository.py
@@ -1,11 +1,12 @@
 from typing import Protocol
 
+from app.domain.subscription.email import Email
 from app.domain.subscription.subscription import Subscription
 
 
 class SubscriptionRepository(Protocol):
     async def add(self, subscription: Subscription) -> None: ...
 
-    async def find_one_by_email(self, email: str) -> Subscription | None: ...
+    async def find_one_by_email(self, email: Email) -> Subscription | None: ...
 
     async def update(self, subscription: Subscription) -> None: ...
--- app/infrastructure/persistence/repository/subscription.py
+++ app/infrastructure/persistence/repository/subscription.py
@@ -1,6 +1,7 @@
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+from app.domain.subscription.email import Email
 from app.domain.subscription.subscription import Subscription
 from app.infrastructure.persistence.schema.subscription import SubscriptionSchema
 
@@ -13,8 +14,10 @@ class SubscriptionRepository:
         self.session.add(SubscriptionSchema.from_domain(subscription))
         await self.session.flush()
 
-    async def find_one_by_email(self, email: str) -> Subscription | None:
-        query = select(SubscriptionSchema).where(SubscriptionSchema.email == email)
+    async def find_one_by_email(self, email: Email) -> Subscription | None:
+        query = select(SubscriptionSchema).where(
+            SubscriptionSchema.email == email.value
+        )
 
         result = await self.session.exec(query)
         subscription = result.one_or_none()
