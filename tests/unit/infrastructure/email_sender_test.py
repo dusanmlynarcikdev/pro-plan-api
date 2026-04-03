d0ef8817b95ac5716a0c123edd057848deeac337
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-04-03 13:58:34 +0200

    add test

--- tests/unit/infrastructure/email_sender_test.py
+++ tests/unit/infrastructure/email_sender_test.py
@@ -1,11 +1,12 @@
 from email.message import EmailMessage
-from unittest.mock import Mock
+from smtplib import SMTP
+from unittest.mock import MagicMock, Mock, patch
 
 from fastapi import BackgroundTasks
 
 from app.application.email.message import Message
 from app.domain.subscription.email import Email
-from app.infrastructure.email_sender import EmailSender
+from app.infrastructure.email_sender import SMTP_DSN, EmailSender
 
 
 def test_send() -> None:
@@ -25,3 +26,19 @@ def test_send() -> None:
     assert email["To"] == "john@doe.com"
     assert email["Subject"] == "subject"
     assert email.get_content().strip() == "body"
+
+
+def test_send_email() -> None:
+    email = EmailMessage()
+    email["To"] = "john@doe.com"
+    email["Subject"] = "subject"
+    email.set_content("body")
+
+    smtp = MagicMock(SMTP)
+    smtp_client = smtp.return_value.__enter__.return_value
+
+    with patch("app.infrastructure.email_sender.SMTP", smtp):
+        getattr(EmailSender, "_EmailSender__send_email")(email)
+
+    smtp.assert_called_once_with(SMTP_DSN)
+    smtp_client.send_message.assert_called_once_with(email)
