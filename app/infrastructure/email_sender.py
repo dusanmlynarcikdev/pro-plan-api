c46cf895cdcc89142e557b831b48cc8f3a6ea43c
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-04-03 13:27:50 +0200

    add email sender test

--- app/infrastructure/email_sender.py
+++ app/infrastructure/email_sender.py
@@ -10,19 +10,19 @@ SMTP_DSN = getenv("SMTP_DSN", "")
 
 
 class EmailSender:
-    def __init__(self, queue: BackgroundTasks) -> None:
-        self.__queue = queue
+    def __init__(self, background_tasks: BackgroundTasks) -> None:
+        self.__background_tasks = background_tasks
 
     def send(self, message: Message) -> None:
         email = self.__create_email(message)
 
-        self.__queue.add_task(self.__send_email, email)
+        self.__background_tasks.add_task(self.__send_email, email)
 
     @staticmethod
     def __create_email(message: Message) -> EmailMessage:
         email = EmailMessage()
-        email["Subject"] = message.subject
         email["To"] = message.recipient.value
+        email["Subject"] = message.subject
         email.set_content(message.body)
 
         return email
---
+++ tests/unit/infrastructure/email_sender_test.py
@@ -0,0 +1,25 @@
+from email.message import EmailMessage
+from unittest.mock import Mock
+
+from fastapi import BackgroundTasks
+
+from app.application.email.message import Message
+from app.domain.subscription.email import Email
+from app.infrastructure.email_sender import EmailSender
+
+
+def test_send() -> None:
+    background_tasks = Mock(BackgroundTasks)
+
+    EmailSender(background_tasks).send(Message(Email("john@doe.com"), "subject", "body"))
+
+    background_tasks.add_task.assert_called_once()
+
+    callback, email = background_tasks.add_task.call_args.args
+
+    assert callback == getattr(EmailSender, "_EmailSender__send_email")
+
+    assert isinstance(email, EmailMessage)
+    assert email["To"] == "john@doe.com"
+    assert email["Subject"] == "subject"
+    assert email.get_content().strip() == "body"
