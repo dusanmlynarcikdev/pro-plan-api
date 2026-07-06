4bca0ea7de537720552312f64f98474cf36a7c44
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-07-06 17:57:06 +0200

    refactor

--- tests/unit/infrastructure/email_sender_test.py
+++ tests/unit/infrastructure/email_sender_test.py
@@ -1,5 +1,5 @@
 from email.message import EmailMessage
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import MagicMock, patch
 
 from aiosmtplib import SMTP
 from fastapi import BackgroundTasks
@@ -10,50 +10,31 @@ from app.domain.subscription.email import Email
 from app.infrastructure.email_sender import EmailSender
 
 
-def test_send() -> None:
-    background_tasks = Mock(BackgroundTasks)
+async def test_send() -> None:
+    background_tasks = BackgroundTasks()
 
-    EmailSender(
+    email_sender = EmailSender(
         background_tasks,
         NameEmail("Acme", "noreply@acme.test"),
         AnyUrl("smtp://john:secret@example.com:465"),
-    ).send(Message(Email("john@doe.com"), "subject", "body"))
+    )
 
-    background_tasks.add_task.assert_called_once()
+    smtp = MagicMock(SMTP)
+    smtp_client = smtp.return_value.__aenter__.return_value
 
-    callback, email = background_tasks.add_task.call_args.args
+    with patch("app.infrastructure.email_sender.SMTP", smtp):
+        email_sender.send(Message(Email("john@doe.com"), "subject", "body"))
 
-    assert callback.__func__ is getattr(EmailSender, "_EmailSender__send_email")
+        await background_tasks()
+
+    smtp.assert_called_once_with(hostname="example.com", port=465, use_tls=True)
+    smtp_client.login.assert_called_once_with("john", "secret")
+
+    smtp_client.send_message.assert_called_once()
+    email = smtp_client.send_message.call_args.args[0]
 
     assert isinstance(email, EmailMessage)
     assert email["From"] == "Acme <noreply@acme.test>"
     assert email["To"] == "john@doe.com"
     assert email["Subject"] == "subject"
     assert email.get_content().strip() == "body"
-
-
-async def test_send_email() -> None:
-    email = EmailMessage()
-    email["To"] = "john@doe.com"
-    email["Subject"] = "subject"
-    email.set_content("body")
-
-    smtp = MagicMock(SMTP)
-    smtp_client = smtp.return_value.__aenter__.return_value
-
-    sender = EmailSender(
-        Mock(BackgroundTasks),
-        NameEmail("Acme", "noreply@acme.test"),
-        AnyUrl("smtp://john:secret@example.com:465"),
-    )
-
-    with patch("app.infrastructure.email_sender.SMTP", smtp):
-        await getattr(sender, "_EmailSender__send_email")(email)
-
-    smtp.assert_called_once_with(
-        hostname="example.com",
-        port=465,
-        use_tls=True,
-    )
-    smtp_client.login.assert_called_once_with("john", "secret")
-    smtp_client.send_message.assert_called_once_with(email)
