e5ddbb99a3fcf0f101a6c93759f1c00a6118ed70
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-07-08 15:25:21 +0200

    add auth

---
+++ app/presentation/api/security.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+
+from app.presentation.api.dependencies import Config
+
+bearer_scheme = HTTPBearer()
+
+
+def check_authentication(
+    config: Config,
+    token: Annotated[HTTPAuthorizationCredentials, Depends(bearer_scheme)],
+) -> None:
+    if config.api_key == token.credentials:
+        return
+
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
