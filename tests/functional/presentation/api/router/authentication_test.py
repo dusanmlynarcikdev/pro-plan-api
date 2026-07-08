a0fa5c90c55b5a064df4176b6a812a87935acc4b
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-07-08 16:43:57 +0200

    refactor test

--- tests/functional/presentation/api/router/authentication_test.py
+++ tests/functional/presentation/api/router/authentication_test.py
@@ -1,53 +1,39 @@
-from typing import Any
-
 import pytest
 from fastapi import status
+from fastapi.routing import APIRoute
 from fastapi.testclient import TestClient
 
-from tests.functional.presentation.api.router.subscription import (
-    create_or_update_test,
-    get_test,
-    renew_test,
-)
+from app.presentation.api.main import app
 
-request_config_parameter = pytest.mark.parametrize(
-    "request_config",
+PUBLIC_PATHS = {"/api/"}
+
+
+request_parameter = pytest.mark.parametrize(
+    "method, url",
     [
-        {
-            "url": create_or_update_test.PATH,
-            "method": "POST",
-            "json": {
-                "email": "john@doe.com",
-                "period": "yearly",
-            },
-        },
-        {
-            "url": get_test.PATH.format(email="john@doe.com"),
-            "method": "GET",
-        },
-        {
-            "url": renew_test.PATH.format(email="john@doe.com"),
-            "method": "POST",
-        },
+        (method, route.path)
+        for route in app.routes
+        if isinstance(route, APIRoute) and route.path not in PUBLIC_PATHS
+        for method in route.methods
     ],
 )
 
 
-@request_config_parameter
-def test_invalid_api_key(client: TestClient, request_config: dict[str, Any]) -> None:
+@request_parameter
+def test_invalid_api_key(client: TestClient, method: str, url: str) -> None:
     client.headers["Authorization"] = "Bearer invalid_api_key"
 
-    response = client.request(**request_config)
+    response = client.request(method, url)
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
     assert response.content == b'{"detail":"Unauthorized"}'
 
 
-@request_config_parameter
-def test_missing_api_key(client: TestClient, request_config: dict[str, Any]) -> None:
+@request_parameter
+def test_missing_api_key(client: TestClient, method: str, url: str) -> None:
     del client.headers["Authorization"]
 
-    response = client.request(**request_config)
+    response = client.request(method, url)
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
     assert response.content == b'{"detail":"Not authenticated"}'
