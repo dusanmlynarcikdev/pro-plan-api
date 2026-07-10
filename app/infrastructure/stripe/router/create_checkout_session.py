46b5909305d3b1a885e120a9a0027b112e4d51b0
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-07-10 15:14:00 +0200

    refactor

--- app/infrastructure/stripe/router/create_checkout_session.py
+++ app/infrastructure/stripe/router/create_checkout_session.py
@@ -1,21 +1,13 @@
 from fastapi import APIRouter, HTTPException, status
 
 from app.domain.subscription.email import Email
-from app.infrastructure.config import get_config
 from app.infrastructure.stripe.client.errors import ClientError
 from app.infrastructure.stripe.router.dependencies import StripeClient
 from app.infrastructure.stripe.router.requests import (
-    BillingPeriod,
     CreateCheckoutSessionRequest,
 )
 from app.infrastructure.stripe.router.responses import UrlResponse
-from app.presentation.api.dependencies import CreateOrGetSubscriptionUseCase
-
-CONFIG = get_config()
-PRICE_IDS = {
-    BillingPeriod.MONTHLY: CONFIG.stripe_price_id_monthly,
-    BillingPeriod.YEARLY: CONFIG.stripe_price_id_yearly,
-}
+from app.presentation.api.dependencies import Config, CreateOrGetSubscriptionUseCase
 
 router = APIRouter()
 
@@ -23,6 +15,7 @@ router = APIRouter()
 @router.post("/stripe/checkout-sessions")
 async def create_checkout_session(
     client: StripeClient,
+    config: Config,
     create_or_get_subscription: CreateOrGetSubscriptionUseCase,
     request: CreateCheckoutSessionRequest,
 ) -> UrlResponse:
@@ -30,7 +23,7 @@ async def create_checkout_session(
 
     try:
         redirect_url = await client.create_checkout_session(
-            PRICE_IDS[request.billing_period], subscription.id
+            request.billing_period.get_stripe_price_id(config), subscription.id
         )
     except ClientError:
         raise HTTPException(
--- app/infrastructure/stripe/router/requests.py
+++ app/infrastructure/stripe/router/requests.py
@@ -2,10 +2,19 @@ from enum import StrEnum
 
 from pydantic import BaseModel
 
+from app.infrastructure.config import Config
+
 
 class BillingPeriod(StrEnum):
     MONTHLY = "monthly"
     YEARLY = "yearly"
+
+    def get_stripe_price_id(self, config: Config) -> str:
+        match self:
+            case BillingPeriod.MONTHLY:
+                return config.stripe_price_id_monthly
+            case BillingPeriod.YEARLY:
+                return config.stripe_price_id_yearly
 
 
 class CreateCheckoutSessionRequest(BaseModel):
