adefe8f3a5282cee8b1c10c5bbb484e5171905b2
Dušan Mlynarčík <dusan.mlynarcik@email.cz>
2026-07-20 10:12:16 +0200

    Creating billing portal session receives customer external id

--- app/application/stripe/billing_portal/create_session_use_case.py
+++ app/application/stripe/billing_portal/create_session_use_case.py
@@ -1,12 +1,20 @@
 from app.application.stripe.billing_portal.client import Client
+from app.domain.customer.repository import CustomerRepository
 
 
 class CreateSessionUseCase:
-    def __init__(self, client: Client) -> None:
+    def __init__(self, client: Client, repository: CustomerRepository) -> None:
         self._client = client
+        self._repository = repository
 
-    async def __call__(self, stripe_customer_id: str) -> str:
+    async def __call__(self, external_customer_id: str) -> str:
         """
+        :raises CustomerNotFound:
         :raises UnableToCreateBillingPortalSessionError:
         """
-        return await self._client.create_session(stripe_customer_id)
+        customer = await self._repository.get_by_external_id(external_customer_id)
+
+        if customer.stripe_id is None:
+            raise RuntimeError("Customer has no stripe id")
+
+        return await self._client.create_session(customer.stripe_id)
--- app/presentation/api/router/stripe/create_billing_portal_session.py
+++ app/presentation/api/router/stripe/create_billing_portal_session.py
@@ -1,20 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.presentation.api.dependencies import CreateStripeBillingPortalSessionUseCase
+from app.presentation.api.responses import create_error_response_doc
 from app.presentation.api.router.stripe.requests import (
-    CreateBillingPortalSessionRequest,
+    CustomerRequest,
 )
 from app.presentation.api.router.stripe.responses import UrlResponse
 
 router = APIRouter()
 
 
-@router.post("/stripe/billing-portal/sessions")
+@router.post(
+    "/stripe/billing-portal/sessions",
+    responses={status.HTTP_404_NOT_FOUND: create_error_response_doc()},
+)
 async def create_billing_portal_session(
     create_session: CreateStripeBillingPortalSessionUseCase,
-    request: CreateBillingPortalSessionRequest,
+    request: CustomerRequest,
 ) -> UrlResponse:
     """
+    :raises CustomerNotFound:
     :raises UnableToCreateBillingPortalSessionError:
     """
-    return UrlResponse(url=await create_session(request.stripe_customer_id))
+    return UrlResponse(url=await create_session(request.customer_external_id))
